ee305a24af6d6b59645916abbade46736688cf15
danteoconnor <dante.oconnor@uconn.edu>
2026-05-12 18:16:01 -0400

    Add CA housing boxplot script and output figure

--- src/boxplot.py
+++ src/boxplot.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib.pyplot as plt #added for plotting
 
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
@@ -12,3 +13,9 @@ print(df.head())
 print(df.shape)
 
 # you can save the boxplot...
+boxplot = pd.plotting.boxplot(df["MedInc"], vert=False)
+plt.title("Median Income Boxplot (In Hundreds of Thousands of Dollars)")
+plt.xlabel("Median Income (In Hundreds of Thousands of Dollars)")
+plt.savefig("figs/boxplot.png")  # Save the boxplot as an image file
+plt.show()
+plt.close()
